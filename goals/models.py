a987df15f791d6b8cdf9c6debf4fe1691e53b561
David Akinola <dakinola54@gmail.com>
2025-03-26 19:11:26 +0100

    created Goal model and the neccesary fields

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Goal(models.Model):
+
+    PRIORITY_CHOICES = [
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High")
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateField()
+    priority = models.CharField(max_length=8, choices=PRIORITY_CHOICES, default="medium")
+
+    def __str__(self):
+        return self.title
