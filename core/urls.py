366cf09e38d03d5e7d9475666e5d25ad2512790f
David Akinola <dakinola54@gmail.com>
2025-03-26 18:49:32 +0100

    customized auth api endpoint

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("dj_rest_auth.urls")),
-    path("api/dj-rest-auth/registration", include("dj_rest_auth.registration.urls"))
+    path("api/auth/signup", include("dj_rest_auth.registration.urls"))
 ]
