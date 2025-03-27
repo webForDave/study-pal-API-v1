2aa9fe0addbfb6c453c40ff5e76f9ff00ae5cc51
David Akinola <dakinola54@gmail.com>
2025-03-27 17:05:42 +0100

    Subject views

--- subjects/views.py
+++ subjects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from .models import Subject
+from .serializers import SubjectSerializer
 
-# Create your views here.
+class SubjectListAPIView(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
