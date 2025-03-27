3a3e7ecda52f2610b04e73e3da90598994413554
David Akinola <dakinola54@gmail.com>
2025-03-27 17:08:37 +0100

    modified model field name

--- subjects/models.py
+++ subjects/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class Subject(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    credits_load = models.IntegerField(blank=True, null=True)
+    credit_units = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
