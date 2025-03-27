250ddca9a6b5ddbbf0e530a738f08ee3c2f5c3ff
David Akinola <dakinola54@gmail.com>
2025-03-27 16:55:43 +0100

    subject model

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    credits_load = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
